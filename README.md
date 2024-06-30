52f30d50072ec0eacab383e48001de45cf57b414
the  develpor <164501586+wasixd@users.noreply.github.com>
2024-06-30 14:59:30 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# WASI-MD-V2-SESSION
+# BELTAH-MD-session-generator
+- Kindly star my repo
+- Fork and edit as you wish
+- Deploy to your favourite hosting server eg Heroku or Render or self hosting
+
+<strong>NB:<strong/> This repo also generates session ID for all bots using whiskeysockets/baileys
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+QR- WEB - PAIR CODE FOR BOT WITH WHISKEYSOCKETS/BAILEYS
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<p align="center">
+   <a href="https://github.com/Beltahmd">
+    
+     
+</a>
+   <a aria-label="QRis free to use" href="https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F" target="_blank">
+ <p align="center"><img src="https://profile-counter.glitch.me/{Brashokish}/count.svg" alt="franceking1:: Visitor's Count" /></p>
+
+
+
+[`Contact Owner`](https://wa.me/254114141192)
+
+FORK THE REPOSITORY (Repo) 
+    <br>
+<a href="https://github.com/Beltahmd/beltah-md-Sessions/fork"><img title="WEB" src="https://img.shields.io/badge/FORK LEGACY-QR?color=black&style=for-the-badge&logo=stackshare"></a>
+
+
+# `Owner`
+
+ <a href="https://github.com/Brashokish"><img src="https://telegra.ph/file/61cc2fe7866ef604d4506.jpg" width="250" height="250" alt="Brasho Kish"/></a>
+
+# `Contributor` 
+<a href="https://github.com/Brashokish">
+**KISH**
+   
---
+++ pair.js
@@ -0,0 +1,105 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Brasho_Kish,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function LEGACY_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Brasho_Kish = Brasho_Kish({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Ubuntu)", "Chrome (Linux)", "Chrome (MacOs)"]
+             });
+             if(!Pair_Code_By_Brasho_Kish.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Brasho_Kish.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Brasho_Kish.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Brasho_Kish.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: '' + b64data });
+
+               let LEGACY_MD_TEXT = `
+*𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐜𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝*
+*𝐄𝐧𝐣𝐨𝐲😺*
+*By _𝐤𝐞𝐢𝐭𝐡𝐤𝐞𝐢𝐳𝐳𝐚𝐡⚪_*
+______________________________
+╔════◇
+║『 𝐘𝐎𝐔'𝐕𝐄 𝐂𝐇𝐎𝐒𝐄𝐍 𝐊𝐄𝐈𝐓𝐇 𝐌𝐃 』
+║ You've Completed the First Step
+║ to Deploy a Whatsapp Bot.
+╚══════════════╝
+╔═════◇
+║ 『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _youtube.com/@keithkeizzah_
+║❍ 𝐎𝐰𝐧𝐞𝐫: _https://wa.me/254748387615_
+║❍ 𝐑𝐞𝐩𝐨: _https://github.com/keithkeizzah/HUNCHO-MD_
+║❍ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _https://chat.whatsapp.com/GvR2YEfyhQQF85jp1rCLNn_
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47_
+║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/keizzah_keith_
+║ ☬ ☬ ☬ ☬
+╚══════════════╝ 
+ 𒂀 𝐊𝐄𝐈𝐓𝐇 𝐌𝐃
+______________________________
+
+Don't Forget To Give Star⭐ To My Repo`
+
+ await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id,{text:LEGACY_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Brasho_Kish.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    LEGACY_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Currently Unavailable"});
+         }
+        }
+    }
+    return await LEGACY_MD_PAIR_CODE()
+});
+module.exports = router
